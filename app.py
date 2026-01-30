d744e4aa3c729cc96cbd3377dc4427c1ba527d01
CJSS1224 <charles.jim.spencer@gmail.com>
2026-01-30 23:44:55 +0800

    SSL CERTS ADDED

--- app.py
+++ app.py
@@ -128,4 +128,9 @@ if __name__ == '__main__':
 ╚══════════════════════════════════════════════════════════════════╝
     """)
     
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, 
+             debug=True, 
+             host='0.0.0.0', 
+             port=5000,
+             certfile='SSL-Certs/localhost-cert.pem',
+             keyfile='SSL-Certs/localhost-key.pem')
